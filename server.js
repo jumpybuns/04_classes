5d42bba29c828593c183ad99c17c9057c3c9f1a2
Jumpybuns <ethanpiercepresents@gmail.com>
2020-12-04 17:59:19 -0800

    postman works

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const { stripInput } = require('.utils.js');
+const { stripInput } = require('./utils.js');
 const app = express();
 const { Stack } = require('./lib/stack.js');
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    console.log(req.body);
+    res.send('yoyoyo');
+})
+
+
 app.post('/lint', (req, res) => {
     const newStack = new Stack();
-    const stringArray = stripInput(req.body.code);
+    const stringArray = stripInput(req.body.lint);
+    let result = true;
+
     stringArray.forEach(char => {
         if (char === '{' || char === '[' || char === '(') {
             newStack.push(char);
@@ -20,7 +28,7 @@ app.post('/lint', (req, res) => {
             } else if (top === '(' && char === ')') {
                 newStack.pop();
             } else {
-                result = `missing ${bracket}`;
+                result = `missing ${char}`;
             };
         }
     }
@@ -30,7 +38,7 @@ app.post('/lint', (req, res) => {
 
 
 app.listen(7890, () => {
-    console.log('farted on 7890')
+    console.log('started on 7890')
 })
 
 
--- utils.js
+++ utils.js
@@ -1,6 +1,8 @@
+const regex = /[(){}\[\]]/g;
+
 function stripInput(input) {
     // console.log(input.match(/[(){}\[\]]/g));
-    return input.match(/[(){}\[\]]/g);
+    return input.match(regex);
 }
 
 module.exports = {
